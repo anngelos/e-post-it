aa8504457ea3d2be9361e7a0c1cd74cd352d0c6e
Ângelo Cardoso <hoanngelos@gmail.com>
2023-08-18 04:46:45 -0300

    posts icons adjusted.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -143,14 +143,14 @@ export default {
           </p>
           <div class="mt-3">
             <button @click="openEditModal(index)"
-              class="mr-3 inline-flex items-center justify-center w-10 h-10 transition-colors duration-150 rounded-full focus:shadow-outline hover:bg-gray-500">
+              class="mr-3 inline-flex items-center justify-center w-8 h-8 transition-colors duration-150 rounded-full focus:shadow-outline icon">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                 <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                 <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
               </svg>
             </button>
             <button @click="removePost(index); storePosts()"
-            class="mr-3 inline-flex items-center justify-center w-10 h-10 transition-colors duration-150 rounded-full focus:shadow-outline hover:bg-gray-500">
+            class="mr-3 inline-flex items-center justify-center w-8 h-8 transition-colors duration-150 rounded-full focus:shadow-outline icon">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg"
                 viewBox="0 0 16 16">
                 <path
@@ -159,7 +159,7 @@ export default {
             </button>
 
             <button @click="copyText()"
-            class="mr-3 inline-flex items-center justify-center w-10 h-10 transition-colors duration-150 rounded-full focus:shadow-outline hover:bg-gray-500">
+            class="mr-3 inline-flex items-center justify-center w-8 h-8 transition-colors duration-150 rounded-full focus:shadow-outline icon">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard2"
                 viewBox="0 0 16 16">
                 <path
@@ -200,5 +200,11 @@ export default {
 
 .ghost-drag {
   visibility: hidden;
+}
+
+.icon:hover {
+  -xpedu-transform: scale(1.3);
+        -ms-transform: scale(1.3);
+        transform: scale(1.3);
 }
 </style>
