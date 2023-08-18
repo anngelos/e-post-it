6b50949d47d4774a2c8a5bdb7a295bfcf89ae6cd
Ângelo Cardoso <hoanngelos@gmail.com>
2023-08-17 23:04:24 -0300

    refactored menu.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,47 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <nav class="bg-gray-700">
-      <div class="container mx-auto py-4 flex justify-between items-center">
-        <h1 class="text-2xl font-bold text-gray-50">ePost-It</h1>
-        <div class="flex space-x-10">
-          <div class="flex items-center space-x-2">
-            <span class="text-gray-50"><RouterLink to="/" exact>Home</RouterLink></span>
+  <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <div class="flex items-center">
+        <img src="./../public/note-512.png" class="h-8 mr-3"/>
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">ePost-It</span>
+      </div>
+      <div class="flex md:order-2">
+        <div class="relative md:block">
+          <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+            <span class="sr-only">Search icon</span>
           </div>
-          <div class="flex items-center space-x-2">
-            <span class="text-gray-50"><RouterLink to="/about">Sobre</RouterLink></span>
-          </div>
+          <input type="text" id="search-navbar"
+            class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Procurar..."
+          />
         </div>
       </div>
-    </nav>
-  </header>
+      <div class="items-center justify-between w-full md:flex md:w-auto md:order-1 menu-container" id="navbar-search">
+        <ul
+          class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 menu-links"
+        >
+          <li>
+            <RouterLink to="/" exact>Home</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/about">About</RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
   <RouterView/>
 </template>
